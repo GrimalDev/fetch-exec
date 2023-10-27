5d94785a9d67432ff6a4bc509891a9f27c8d34e3
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-10-27 16:17:08 +0200

    Update fetch-exec.sh
    the fetch returns the error code

--- fetch-exec.sh
+++ fetch-exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-html="$(curl -X GET \
+response=$(curl -X GET \
   -H 'Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5' \
   -H 'Accept: application/json' \
   -H 'Cache-Control: max-age=0' \
@@ -14,13 +14,26 @@ html="$(curl -X GET \
   -A 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' \
   --location \
   --compressed \
-  --referer \
   --connect-timeout 120 \
   --max-time 30 \
   --max-redirs 10 \
   --insecure \
   --http1.1 \
   --silent \
-  $1)"
+  --write-out "[[END]]%{http_code}" \
+  "$1")
 
-echo $html > $2
+#seperate the response from the http code
+fetchResult="${response%\[\[END]]*}"
+code="${response##*\[\[END]]}"
+
+#Check if the code is something other than 200 or response empty
+if [ "$code" != "200" ] || [ -z "$fetchResult" ]; then
+  echo "[{ \"httpCode\": \"$code\" }]"
+  exit 1
+fi
+
+echo "[{ \"httpCode\": \"$code\" }]"
+
+#Save the response to a file
+echo "$fetchResult" > "$2"
